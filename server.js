2a1ac80e3e3c814d6bda4b5b2172abd3c6be15eb
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-11-11 12:09:49 -0800

    server respond for /secret

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ app.get('/*', function(req, res) {
   res.status(404).send('This page could not be found.');
 });
 
+app.get('/secret', function(req, res) {
+  res.status(500).sendFile(__dirname + '/app/secret.html');
+});
+
 app.use(function(req, res, next) {
-  res.status(404).sendFile(__dirname + '/app/404.html')
-})
+  res.status(404).sendFile(__dirname + '/app/404.html');
+});
 
 app.listen(port, function() {
   console.log('server running on port' + port);
