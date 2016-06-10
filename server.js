4bb5a3e7f13291c73c8399d3792d0aedbeaafd41
Farhad Ahmed <farhadahmed2007@gmail.com>
2016-06-09 20:06:35 -0700

    Fix syntax error in server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var port = process.env.PORT || 3000
 app.use(express.static(__dirname + '/app'));
 
 app.get('/V2', function(req, res) {
-  res.status(500).sendFile(__dirname) + '/app/indexV2.html');
-}
+  res.status(500).sendFile(__dirname + '/app/indexV2.html');
+});
 
 app.get('/secret', function(req, res) {
   res.status(500).sendFile(__dirname + '/app/secret.html');
