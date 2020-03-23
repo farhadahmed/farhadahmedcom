003f86076240d97a5999a915763cb73e7b2dc8ae
Farhad Ahmed <fahmed@wa-fahmed-mbp.local>
2020-03-23 02:44:31 -0700

    v2 ready

---
+++ app/js/animated-skills.js
@@ -0,0 +1,9 @@
+(function( $ ){
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},3000);
+	});
+});
+})(jQuery);
